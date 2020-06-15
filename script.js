370a260ee838ba6cbe129e47c7f07db836adbab1
Bunea Rares <bunearaes@gmail.com>
2020-06-16 02:01:58 +0300

    updated the code,working on a game stop function

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const drawn = "Darwn!"
 let numberOfGames = 0;
 let playerScore = 0;
 let computerScore = 0;
+let playerRock = document.getElementById('rock');
+let playerPaper = document.getElementById('paper');
+let playerScissor = document.getElementById('scissor');
+let scoreHistory = document.getElementById('score_output');
+let rounds = 1;
+
 
 let computerPlay = () =>{
     let computerSelection = Math.floor(Math.random() * 3);
@@ -17,50 +23,83 @@ let computerPlay = () =>{
     }
 }
 
+
+
 let round = (playerSelection,computerSelection) =>{
     playerSelection = playerSelection.toLowerCase();
     console.log(playerSelection);
     console.log(computerSelection);
     if(playerSelection === computerSelection){
-        console.log(`${playerScore} : ${computerScore}`)
+        console.log(`${playerScore} : ${computerScore}`);
+        gameMatch(rounds+=1);
         return drawn;
     }
     if(playerSelection === `rock`){
         if(computerSelection === `paper`){
             console.log(`${playerScore} : ${computerScore+=1}`);
+            gameMatch(rounds+=1);
             return lose;
         }console.log(`${playerScore+=1} : ${computerScore}`);
+        gameMatch(rounds+=1);
         return win;
     }
     if(playerSelection === `paper`){
         if(computerSelection === `rock`){
-            console.log(`${playerScore} : ${computerScore+=1}`);
+            console.log(`${playerScore+=1} : ${computerScore}`);
+            gameMatch(rounds+=1);
             return win;
         }console.log(`${playerScore} : ${computerScore+=1}`);
+        gameMatch(rounds+=1);
         return lose;
     }
     if(playerSelection === `scissor`){
         if(computerSelection === `rock`){
             console.log(`${playerScore} : ${computerScore+=1}`);
+            gameMatch(rounds+=1);
             return lose;
         }console.log(`${playerScore+=1} : ${computerScore}`);
+        gameMatch(rounds+=1);
         return win;
     }
 }
 
+let startGame
 
 
 let gameMatch = (rounds = 1) => {
-    while(rounds < 6){
-        console.log(`Round ${rounds} out of 5`);
-        round(prompt(`Choose`),computerPlay());
-        rounds++;
-    }
-    if(playerScore>computerScore){
-        return `You won!`;
-    }else if(playerScore<computerScore){
-        return `You lost`;
-    }else{return `It's a drawn!`}
+   if(rounds == 6){
+      finalScore(playerScore,computerScore)
+   }
 }
 
-console.log(gameMatch())
+let finalScore = (playerScore,computerScore) => {
+    if(playerScore === computerScore){
+        return gameStop();
+        
+    }else if(playerScore > computerScore){
+        return gameStop();
+        
+    }return gameStop();
+    
+}
+
+
+let gameRestart = () => location.reload();
+
+
+let gameStarter = (playerInput) =>{
+round(playerInput,computerPlay());
+}
+
+let gameStop = () => {};
+
+playerRock.addEventListener(`click`, () =>{
+    gameStarter(playerRock.id);
+})
+playerPaper.addEventListener(`click`, () =>{
+    gameStarter(playerPaper.id);
+})
+playerScissor.addEventListener(`click`, () =>{
+    gameStarter(playerScissor.id);
+})
+console.log(playerRock);
