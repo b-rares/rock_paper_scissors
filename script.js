3cfabf27dcaf02d26b732127b83a54cf49620816
Bunea Rares <bunearaes@gmail.com>
2020-06-16 19:47:31 +0300

    Added a function that pushes the score into the html(it's not working) and cleared the code.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const win = "You won!";
-const lose = "You lost.";
-const drawn = "Darwn!"
+let win = 0;
+let lose = 0;
+const drawn = 0;
+const playerRock = document.querySelector(`#rock`);
+const playerPaper = document.querySelector("#paper");
+const playerScissor = document.querySelector("#scissor");
+const playerRestart = document.getElementById(`reset`);
+const scoreOutput = document.getElementById('p')
 let numberOfGames = 0;
 let playerScore = 0;
 let computerScore = 0;
-let playerRock = document.getElementById('rock');
-let playerPaper = document.getElementById('paper');
-let playerScissor = document.getElementById('scissor');
-let scoreHistory = document.getElementById('score_output');
-let rounds = 1;
-
+let liContent = document.createTextNode(``);
+let newLi = document.createElement('LI');
+let scoreContainer = document.getElementById('score_container');
 
 let computerPlay = () =>{
     let computerSelection = Math.floor(Math.random() * 3);
@@ -24,82 +26,80 @@ let computerPlay = () =>{
 }
 
 
-
 let round = (playerSelection,computerSelection) =>{
-    playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
+    console.log(playerSelection.id);
     console.log(computerSelection);
-    if(playerSelection === computerSelection){
-        console.log(`${playerScore} : ${computerScore}`);
-        gameMatch(rounds+=1);
-        return drawn;
+    if(playerSelection.id === computerSelection){
+        liContent = `You both chose ${computerSelection},it's a drawn!`;
+        matchHistory(liContent);
+        return drawn
     }
-    if(playerSelection === `rock`){
+    if(playerSelection.id === `rock`){
         if(computerSelection === `paper`){
-            console.log(`${playerScore} : ${computerScore+=1}`);
-            gameMatch(rounds+=1);
-            return lose;
-        }console.log(`${playerScore+=1} : ${computerScore}`);
-        gameMatch(rounds+=1);
-        return win;
+            liContent = `You chose ${playerSelection.id},but ${computerSelection} beats you!`;
+            matchHistory(liContent);
+            return lose+=1;
+        }liContent = `You chose ${playerSelection.id} and beated the ${computerSelection}`
+        matchHistory(liContent);
+        return win+=1;
     }
-    if(playerSelection === `paper`){
+    if(playerSelection.id === `paper`){
         if(computerSelection === `rock`){
-            console.log(`${playerScore+=1} : ${computerScore}`);
-            gameMatch(rounds+=1);
-            return win;
-        }console.log(`${playerScore} : ${computerScore+=1}`);
-        gameMatch(rounds+=1);
-        return lose;
+            liContent = `You chose ${playerSelection.id} and beated the ${computerSelection}`
+            matchHistory(liContent);
+            return win+=1;
+        }liContent = `You chose ${playerSelection.id},but ${computerSelection} beats you!`;
+        matchHistory(liContent);
+        return lose+=1;
     }
-    if(playerSelection === `scissor`){
+    if(playerSelection.id === `scissor`){
         if(computerSelection === `rock`){
-            console.log(`${playerScore} : ${computerScore+=1}`);
-            gameMatch(rounds+=1);
-            return lose;
-        }console.log(`${playerScore+=1} : ${computerScore}`);
-        gameMatch(rounds+=1);
-        return win;
+            liContent = `You chose ${playerSelection.id},but ${computerSelection} beats you!`;
+            matchHistory(liContent);
+            return lose+=1;
+        }
+        liContent = `You chose ${playerSelection.id} and beated the ${computerSelection}`;
+        matchHistory(liContent);
+        return win+=1;
     }
 }
 
-let startGame
-
-
-let gameMatch = (rounds = 1) => {
-   if(rounds == 6){
-      finalScore(playerScore,computerScore)
-   }
+let matchHistory = (liContent) => {
+    if(numberOfGames <= 5){
+        newLi.appendChild(liContent);
+        scoreContainer.appendChild(newLi);
+    }else if(win>lose){
+        scoreContainer.appendChild(`You have won!If you wish to play again,please hit Restart`)
+    }else if(win<lose){
+        scoreContainer.appendChild(`You have lost.Try your luck again hitting Restart`)
+    }else{
+        scoreContainer.appendChild(`You both are awsome,it's a drawn!Try again hitting Restart`)
+    }
 }
-
-let finalScore = (playerScore,computerScore) => {
-    if(playerScore === computerScore){
-        return gameStop();
-        
-    }else if(playerScore > computerScore){
-        return gameStop();
-        
-    }return gameStop();
-    
-}
-
 
 let gameRestart = () => location.reload();
 
 
 let gameStarter = (playerInput) =>{
 round(playerInput,computerPlay());
+numberOfGames++;
+if(numberOfGames > 5){
+    playerRock.querySelector("button").disabled = true;
+    playerScissor.querySelector("button").disabled = true;
+    playerPaper.querySelector("button").disabled = true;
+}
 }
 
-let gameStop = () => {};
 
 playerRock.addEventListener(`click`, () =>{
-    gameStarter(playerRock.id);
+    gameStarter(playerRock);
 })
 playerPaper.addEventListener(`click`, () =>{
-    gameStarter(playerPaper.id);
+    gameStarter(playerPaper);
 })
 playerScissor.addEventListener(`click`, () =>{
-    gameStarter(playerScissor.id);
+    gameStarter(playerScissor);
 })
-console.log(playerRock);
+playerRestart.addEventListener('click', () => {
+   gameRestart();
+})
